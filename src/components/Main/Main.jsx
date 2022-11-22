638d306ec7f4ae6e25a784283cf83ff26825defc
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2022-11-22 11:57:24 -0300

    -- Medidas Actualizadas --

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,29 +3,29 @@ import Ingredientes from "../../assets/ingredientes.jpeg";
 import styled from "@emotion/styled";
 
 const H2 = styled.h2`
-    text-align: center;
-    font-weight: 400;
-    width:10rem;
-    margin: 2rem auto 1rem;
-`
+  text-align: center;
+  font-weight: 400;
+  width: 10rem;
+  margin: 2rem auto 1rem;
+`;
 const Img = styled.img`
   height: 20rem;
 `;
 const Main = () => {
-  return (
+   return (
     <section id="ingredientes">
       <H2>Ingredientes Principales</H2>
       <div className="test">
         <Img src={Ingredientes} alt="" />
         <div>
-        <p>- Pan de papa -</p>
-        <p>- Lechuga -</p>
-        <p>- Tomate -</p>
-        <p>- Cheddar -</p>
-        <p>- Medallon de carne -</p>
-        <p>- Cebolla -</p>
-        <p>- Pepino -</p>
-        <p>- Panceta -</p>
+          <p>-Pan de papa-</p>
+          <p>-Lechuga-</p>
+          <p>-Tomate-</p>
+          <p>-Cheddar-</p>
+          <p>-Medallon de carne-</p>
+          <p>-Cebolla-</p>
+          <p>-Pepino-</p>
+          <p>-Panceta-</p>
         </div>
       </div>
     </section>
